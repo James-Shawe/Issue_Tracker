2fd01b6e1f3d199e519a70a538365415e7c78288
JamesShaweAistech <james.shawe@aistechspace.com>
2020-07-17 15:59:39 +0100

    learning about React States

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-const issues = [
+const initialIssues = [
   {
     id: 1, status: 'New', owner: 'Ravan', effort: 5,
     created: new Date('2018-08-15'), due: undefined,
@@ -10,6 +10,10 @@ const issues = [
     title: 'Missing bottom border on panel',
   },
 ];
+
+const sampleIssue = {
+status: 'New', owner: 'Pieta', title: 'Completion date should be optional',
+};
 
 class IssueFilter extends React.Component {
   render() {
@@ -37,8 +41,35 @@ class IssueRow extends React.Component {
 }
 
 class IssueTable extends React.Component {
+
+constructor() {
+super ();
+this.state = {issues: []};
+setTimeout(() => {
+this.createIssue(sampleIssue);
+}, 2000);
+};
+
+  componentDidMount() {
+  this.loadData();
+  }
+
+  loadData() {
+  setTimeout(()=>{
+  this.setState({issues: initialIssues});
+  }, 500);
+  };
+
+  createIssue (issue) {
+  issue.id = this.state.issues.length + 1;
+  issue.created = new Date();
+  const newIssueList = this.state.issues.slice();
+  newIssueList.push(issue);
+  this.setState({issues: newIssueList});
+  };
+
   render() {
-    const issueRows = issues.map(issue =>
+    const issueRows = this.state.issues.map(issue =>
       <IssueRow key={issue.id} issue={issue} />
     );
 
